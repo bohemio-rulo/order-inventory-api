a52ccfa14181ffa514a14903abdf51528c23536d
bohemio-rulo <bohemiorulo@gmail.com>
2026-04-28 15:45:14 -0400

    add swagger

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use OpenApi\Attributes as OA;
 
-#[OA\Info(title: "My API", version: "0.1")]
+#[OA\Info(title: "Api Order", version: "0.1")]
 class OrderController extends Controller
 {
     #[OA\Post(
         path: '/api/orders',
-        summary: 'Create an order',
+        summary: 'Create order',
         tags: ['Orders']
+    )]
+    #[OA\RequestBody(
+        required: true,
+        content: new OA\JsonContent(ref: "#/components/schemas/Order") // reference to model
     )]
     #[OA\Response(
         response: 201,
-        description: 'Order created'
+        description: 'Order created',
+        content: new OA\JsonContent(ref: "#/components/schemas/Order")
     )]
     public function store()
     {
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OpenApi\Attributes as OA;
 
+#[OA\Schema(
+    title: "Order",
+    description: "Model to Order",
+    required: ["product_id", "quantity"]
+)]
 class Order extends Model
 {
-    //
+    #[OA\Property(property: "id", type: "integer", readOnly: true, example: 1)]
+    private $id;
+
+    #[OA\Property(property: "product_id", type: "integer", description: "ID product", example: 10)]
+    protected $product_id;
+
+    #[OA\Property(property: "quantity", type: "integer", example: 5)]
+    protected $quantity;
+
+    #[OA\Property(property: "status", type: "string", enum: ["pending", "completed"], example: "pending")]
+    protected $status;
 }
