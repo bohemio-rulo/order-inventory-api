ff966058e7d5be31a3a778fc6ad9a3f6e20fc9d3
bohemio-rulo <bohemiorulo@gmail.com>
2026-04-28 16:15:58 -0400

    feat(resource): agrega OrderResource para estructurar respuestas limpias de ordenes

---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'product_id' => $this->product_id,
+            'quantity' => $this->quantity,
+            'status' => $this->status,
+        ];
+    }
+}
