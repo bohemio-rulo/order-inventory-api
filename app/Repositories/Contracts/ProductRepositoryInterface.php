7b17d11443bf8cff5991b4c371f2ecb5431be567
bohemio-rulo <bohemiorulo@gmail.com>
2026-04-28 21:20:26 -0400

    feat(repository): amplía ProductRepository y su interfaz con métodos CRUD (all, create, update, delete)

--- app/Repositories/Contracts/ProductRepositoryInterface.php
+++ app/Repositories/Contracts/ProductRepositoryInterface.php
@@ -6,6 +6,10 @@ use App\Models\Product;
 
 interface ProductRepositoryInterface
 {
+    public function all(): array;
+    public function create(array $data): Product;
+    public function update(Product $product, array $data): Product;
+    public function delete(Product $product): void;
     public function findById(int $id): Product;
     public function decreaseStock(Product $product, int $qty): void;
 }
--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -9,6 +9,46 @@ use App\Repositories\Contracts\ProductRepositoryInterface;
 class ProductRepository implements ProductRepositoryInterface
 {
     /**
+     * Obtiene todos los productos.
+     * @return Product[]
+     */
+    public function all(): array
+    {
+        return Product::all()->all();
+    }
+
+    /**
+     * Crea un producto.
+     * @param array $data
+     * @return Product
+     */
+    public function create(array $data): Product
+    {
+        return Product::create($data);
+    }
+
+    /**
+     * Actualiza un producto existente
+     * @param Product $product
+     * @param array $data
+     * @return Product
+     */
+    public function update(Product $product, array $data): Product
+    {
+        $product->update($data);
+        return $product;
+    }
+
+    /**
+     * Elimina un producto.
+     * @param Product $product
+     */
+    public function delete(Product $product): void
+    {
+        $product->delete();
+    }
+
+    /**
      * @param int $id
      * @return Product
      */
